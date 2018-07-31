0109f1e1a9187a5db66aa971f60e2ab57788e96d
eveporcello <eve@moonhighway.com>
2018-07-30 19:28:53 -0700

    Adds test

---
+++ src/__tests__/query-me.js
@@ -0,0 +1,34 @@
+import { ApolloServer } from 'apollo-server'
+import { readFileSync } from 'fs'
+import resolvers from '../../src-api/resolvers'
+import { request } from 'graphql-request'
+
+describe("Application Queries", () => {
+
+    let server
+
+    beforeAll(() => {
+        const typeDefs = readFileSync('./src-api/typeDefs.graphql', 'UTF-8')
+        server = new ApolloServer({ typeDefs, resolvers })
+        server.listen(3000)
+    })
+
+    afterAll(() => {
+        server.stop()
+    })
+
+    describe("Me Query", async () => {
+        it("returns null when there is not a current user", async () => {
+            let { me } = await request('http://localhost:3000', `
+                query { 
+                    me { 
+                        name 
+                    } 
+                }
+            `)
+
+            expect(me).toEqual(null)
+        })
+    })
+
+})
