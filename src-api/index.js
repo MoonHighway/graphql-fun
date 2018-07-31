e669b526c11fc73cafe8d7293149d19b428df780
eveporcello <eve@moonhighway.com>
2018-07-31 16:52:01 -0700

    remove notes from index

--- src-api/index.js
+++ src-api/index.js
@@ -1,34 +1,3 @@
-//
-//   TODO: Figure out Heroku Bug
-//
-//      [x] Get a simple server running on Heroku
-//         * Replace the root index.js file with simple ApolloSerer code
-//         * Does a simple "hello world" server have the same issue?
-//          ** Note ** had to add env variable for development. Playground wont run w/out that
-//      [ x ] Get a simplewebpack build server running on Heroku
-//         * [ x] Move the code from the root index to this file
-//         * [ x ] Replace the "require('module')" with "import"
-//         * [ x ] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//      [ x ] Import the typeDefs
-//         * [ x ] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//      [x] Import the Resolvers
-//         * [x ] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//      [x] Add Mocks
-//         * [x ] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//      [x] Upgrade to apollo-server-express
-//         * [x] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//      [x ] Serve the Static ./build folder
-//         * [x] Build the root index and run on Heroku
-//         * Does a simple "hello world" server have the same issue?
-//
-
-// Insert Code Here...
-
 import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
 import typeDefs from './typeDefs.graphql'
