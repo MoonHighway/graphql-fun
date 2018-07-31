ed1bc2af0292875a4abd353d0668639a35dec647
Eve Porcello <eve@moonhighway.com>
2018-07-30 18:28:45 -0700

    Working environment (#1)
    * Adds schema
    * adds Apollo Server setup
    * Adds client setup including serving build folder and commands
    * sets up dev command with concurrently
    * tweaks to API and package.json scripts

---
+++ src-api/index.js
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import typeDefs from './typeDefs.graphql'
+import expressPlayground from 'graphql-playground-middleware-express'
+import resolvers from './resolvers'
+
+const app = express()
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    mocks: true
+})
+
+server.applyMiddleware({ app })
+
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+app.use('/', express.static('./build'))
+
+app.listen({ port: process.env.PORT || 3000 }, () =>
+    console.log(`GraphQL Fun Running`))
+
