3975fad7410b3b51f1ce8c361d224a70392b4595
eveporcello <eve@moonhighway.com>
2018-08-01 12:04:38 -0700

    remove tests

--- src/__tests__/Connections.test.js
+++ src/__tests__/Connections.test.js
@@ -1,44 +1,44 @@
-import { ApolloServer } from 'apollo-server'
-import { readFileSync } from 'fs'
-import resolvers from '../../src-api/resolvers'
-import { request } from 'graphql-request'
-import players from './data/test-users.json'
+// import { ApolloServer } from 'apollo-server'
+// import { readFileSync } from 'fs'
+// import resolvers from '../../src-api/resolvers'
+// import { request } from 'graphql-request'
+// import players from './data/test-users.json'
 
-describe("Connections", () => {
+// describe("Connections", () => {
 
-    let server
-    const typeDefs = readFileSync('./src-api/typeDefs.graphql', 'UTF-8')
+//     let server
+//     const typeDefs = readFileSync('./src-api/typeDefs.graphql', 'UTF-8')
 
-    beforeAll(() => {
-        const context = { players }
-        server = new ApolloServer({ typeDefs, resolvers, context })
-        server.listen(3000)
-    })
+//     beforeAll(() => {
+//         const context = { players }
+//         server = new ApolloServer({ typeDefs, resolvers, context })
+//         server.listen(3000)
+//     })
 
-    afterAll(() => {
-        server.stop()
-    })
+//     afterAll(() => {
+//         server.stop()
+//     })
 
-    it("returns the correct players", async () => {
-        let response = await request('http://localhost:3000', `
-            query {
-                playerCount
-                allPlayers {
-                  login
-              }
-            }
-        `)
+//     it("returns the correct players", async () => {
+//         let response = await request('http://localhost:3000', `
+//             query {
+//                 playerCount
+//                 allPlayers {
+//                   login
+//               }
+//             }
+//         `)
 
-        let expectedResponse = {
-            playerCount: 3,
-            allPlayers: [
-                { login: "bill" },
-                { login: "jill" },
-                { login: "will" }
-            ]
-        }
+//         let expectedResponse = {
+//             playerCount: 3,
+//             allPlayers: [
+//                 { login: "bill" },
+//                 { login: "jill" },
+//                 { login: "will" }
+//             ]
+//         }
 
-        expect(response).toEqual(expectedResponse)
-    })
+//         expect(response).toEqual(expectedResponse)
+//     })
 
-})
+// })
