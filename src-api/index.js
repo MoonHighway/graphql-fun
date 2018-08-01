67f41c801e8a17a87d4fd20043e319c60c4275ef
MoonTahoe <alex@moonhighway.com>
2018-07-31 18:17:04 -0700

    Adds production and development environents and variables

--- src-api/index.js
+++ src-api/index.js
@@ -4,6 +4,16 @@ import typeDefs from './typeDefs.graphql'
 import expressPlayground from 'graphql-playground-middleware-express'
 import resolvers from './resolvers'
 
+console.log('\n\nenvironment variables\n=================')
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('REACT_APP_GRAPHQL_ENDPOINT', process.env.REACT_APP_GRAPHQL_ENDPOINT)
+console.log('REACT_APP_GRAPHQL_SUBSCRIPTIONS', process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS)
+console.log('REACT_APP_GITHUB_CLIENT_ID', process.env.REACT_APP_GITHUB_CLIENT_ID)
+console.log('GITHUB_CLIENT_SECRET', process.env.GITHUB_CLIENT_SECRET)
+console.log('REACT_APP_MAX_CONNECTIONS', process.env.REACT_APP_MAX_CONNECTIONS)
+console.log('REACT_APP_WEJAY_MAX_FACES', process.env.REACT_APP_WEJAY_MAX_FACES)
+console.log('=================\n\n')
+
 const app = express()
 
 const server = new ApolloServer({
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+console.log('\n\nenvironment variables\n=================')
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('REACT_APP_GRAPHQL_ENDPOINT', process.env.REACT_APP_GRAPHQL_ENDPOINT)
+console.log('REACT_APP_GRAPHQL_SUBSCRIPTIONS', process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS)
+console.log('REACT_APP_GITHUB_CLIENT_ID', process.env.REACT_APP_GITHUB_CLIENT_ID)
+console.log('REACT_APP_MAX_CONNECTIONS', process.env.REACT_APP_MAX_CONNECTIONS)
+console.log('REACT_APP_WEJAY_MAX_FACES', process.env.REACT_APP_WEJAY_MAX_FACES)
+console.log('=================\n\n')
+
 render(<h1>GraphQL Fun</h1>, document.getElementById('root'))
