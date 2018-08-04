28e6c2f7547891988aee4067146b5d5f2e13122b
eveporcello <eve@moonhighway.com>
2018-08-03 22:14:14 -0700

    Adds GraphQL fun homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { PlayerScreen } from './components/PlayerScreen'
+import { injectGlobal, ThemeProvider } from 'styled-components'
+import indieFlower from './assets/IndieFlower.ttf'
+import headFont from './assets/Oswald-Bold.ttf'
+import txtFont from './assets/Oswald-Light.ttf'
+import { theme } from './config.json'
 
 console.log('\n\nenvironment variables\n=================')
 console.log('NODE_ENV', process.env.NODE_ENV)
@@ -10,4 +16,51 @@ console.log('REACT_APP_MAX_CONNECTIONS', process.env.REACT_APP_MAX_CONNECTIONS)
 console.log('REACT_APP_WEJAY_MAX_FACES', process.env.REACT_APP_WEJAY_MAX_FACES)
 console.log('=================\n\n')
 
-render(<a href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&scope=user`}>Get Github Code</a>, document.getElementById('root'))
+injectGlobal`
+  @font-face {
+    font-family: 'Indie Flower';
+    font-style: normal;
+    font-weight: 400;
+    src: url(${indieFlower}) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  }
+  @font-face {
+    font-family: 'HeadingFont';
+    src: url(${headFont}) format('opentype');
+  }
+  @font-face {
+    font-family: 'TextFont';
+    src: url(${txtFont}) format('opentype');
+  }
+  html, 
+  body, 
+  #root {
+        height: 100%;
+  }
+  body {
+    display: flex;
+    min-height: 100%;
+    height: auto;
+    padding: 0;
+    margin: 0;
+  }
+  #root {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100%;
+    height: auto;
+  }
+  h1, h2, h3, h4, p, td, span, div {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+render(
+    <ThemeProvider theme={theme}>
+        <PlayerScreen />
+    </ThemeProvider>,
+    document.getElementById('root')
+)
