087bc7fcfdb41660b921fa1c8a78c74271449550
MoonTahoe <alex@moonhighway.com>
2018-08-03 21:41:49 -0700

    Basic Wejay functionality

--- src-api/resolvers/Game.js
+++ src-api/resolvers/Game.js
@@ -26,19 +26,47 @@ export const Mutation = {
         playersOnDeck = null
         return true
     },
-    play: (root, args, { currentGame, currentPlayer }) => {
-        let { instrument } = currentGame.players.find(p => p.login === currentPlayer.login)
-        if (!currentGame.playingMusic.includes(instrument)) {
-            currentGame.playingMusic.push(instrument)
+    play: (root, args, { currentGame, currentPlayer, pubsub }) => {
+
+        let musician = currentGame.players.find(p => p.login === currentPlayer.login)
+
+        if (musician) {
+            if (!currentGame.playingMusic.map(m => m.login).includes(musician.login)) {
+                currentGame.playingMusic.push(musician)
+                pubsub.publish('game-changer', { gameChange: currentGame })
+            }
+        } else {
+            if (!currentGame.faces.map(m => m.login).includes(currentPlayer.login)) {
+                currentGame.faces.push(currentPlayer)
+                pubsub.publish('game-changer', { gameChange: currentGame })
+            }
         }
+        
         return true
     },
-    pause: (root, args, { currentGame, currentPlayer }) => {
-        let { instrument } = currentGame.players.find(p => p.login === currentPlayer.login)
-        if (currentGame.playingMusic.includes(instrument)) {
-            currentGame.playingMusic = currentGame.playingMusic.filter(i => i !== instrument)
-        }
+    pause: (root, args, { currentGame, currentPlayer, pubsub }) => {
 
+        let musician = currentGame.players.find(p => p.login === currentPlayer.login)
+
+        if (musician) {
+            if (currentGame.playingMusic.map(p=>p.login).includes(musician.login)) {
+                currentGame.playingMusic = currentGame.playingMusic.filter(p => p.login !== musician.login)
+                pubsub.publish('game-changer', { gameChange: currentGame })
+            }
+        } else {
+            if (currentGame.faces.map(f => f.login).includes(currentPlayer.login)) {
+                currentGame.faces = currentGame.faces.filter(p => p.login !== currentPlayer.login)
+                pubsub.publish('game-changer', { gameChange: currentGame })
+            }
+        }
+        
         return true
+
+    }
+}
+
+export const Subscription = {
+    gameChange: {
+        subscribe: (_, args, { pubsub }) => pubsub.asyncIterator('game-changer')
     }
 }
--- src-api/resolvers/SelectPlayer.js
+++ src-api/resolvers/SelectPlayer.js
@@ -4,18 +4,18 @@ export const Mutation = {
             players.forEach(p => availablePlayers.push(p))
         }
         let randomId = Math.floor(Math.random() * availablePlayers.length)
-        let [player] = availablePlayers.splice(randomId, 1)
+        let [player] = global.availablePlayers.splice(randomId, 1)
         playersOnDeck.push(player)
         return { count: playersOnDeck.length, player }
     },
     putBackPlayer(root, { login }, { playersOnDeck }) {
         if (login) {
-            var player = playersOnDeck.find(p => p.login === login)
+            var player = global.playersOnDeck.find(p => p.login === login)
             if (!player) {
                 throw new Error("No one with that login exists!")
             }
         } else {
-            var player = playersOnDeck.pop()
+            var player = global.playersOnDeck.pop()
         }
         return { count: playersOnDeck.length, player }
     }
