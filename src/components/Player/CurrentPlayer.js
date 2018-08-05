4d4ae20fd80660f9842c061b8293dd2cbfebfa89
eveporcello <eve@moonhighway.com>
2018-08-05 13:48:14 -0700

    Lists players by team

--- src/components/Player/CurrentPlayer.js
+++ src/components/Player/CurrentPlayer.js
@@ -63,5 +63,3 @@ export class CurrentPlayer extends Component {
             </div>
     }
 }
-
-
--- src/components/Player/Team.js
+++ src/components/Player/Team.js
@@ -5,7 +5,7 @@ export const Team = ({ color, players }) => (
         <h1>{color}</h1>
         <ul>
             {players.map((p, i) =>
-                <li key={i}>{p.login}</li>
+                <li><img src={p.avatar} width={48} height={48} alt="" key={i} />{p.login}</li>
             )}
         </ul>
     </div>
