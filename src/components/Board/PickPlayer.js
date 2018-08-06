b51f36713d7af49f6155807b65a644b235d5b06c
eveporcello <eve@moonhighway.com>
2018-08-05 17:58:10 -0700

    Pick Player with Screen change working

--- src/components/Board/PickPlayer.js
+++ src/components/Board/PickPlayer.js
@@ -1,4 +1,38 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { Mutation } from 'react-apollo'
 
-export const PickPlayer = () =>
-    <h1>Pick Player</h1>
+const PICK_PLAYER = gql`
+    mutation pickPlayer {
+        pickPlayer {
+            count
+            player {
+                login
+                avatar
+            }
+        }
+    }
+`
+
+const PlayerPicked = ({ pickPlayer }) => {
+    return <div>
+        <h1>{pickPlayer.player.login}</h1>
+        <img src={pickPlayer.player.avatar} alt="" width={100} height={100} />
+    </div >
+}
+
+
+export const PickPlayer = () => (
+    <Mutation mutation={PICK_PLAYER}>
+        {(mutation, { data }) => {
+            return (
+                <div>
+                    {!data ? <h1>Come on Down</h1> : <PlayerPicked {...data} />}
+                    <button onClick={mutation}>Pick Player</button>
+                </div>
+            )
+        }
+        }
+
+    </Mutation >
+)
