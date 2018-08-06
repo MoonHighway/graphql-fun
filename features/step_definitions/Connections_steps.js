650b89d1a89b853021ae7d6c77283fbede13adb7
MoonTahoe <alex@moonhighway.com>
2018-08-06 16:01:01 -0700

    Returning to origional test config

--- features/step_definitions/Connections_steps.js
+++ features/step_definitions/Connections_steps.js
@@ -4,22 +4,13 @@ const fetch = require('node-fetch')
 let results
 
 When('I send the following operation:', function async (query, done) {
-
-    fetch('http://localhost:8080')
-        .then(res => console.log('successful fetch'))
+    this.request(this.endpoint, query)
+        .then(r => results = r)
         .then(() => done())
-        .catch(error => { 
-            console.log('error: ', error.message) 
-            done()
-        })
-
-    // this.request(this.endpoint, query)
-    //     .then(r => results = r)
-    //     .then(() => done())
-    //     .catch(done)
+        .catch(done)
 })
 
 Then('I should recieve the following data payload:', function (jsonString) {
-    // let expected = JSON.parse(jsonString)
-    // this.expect(results).to.deep.equal(expected)
+    let expected = JSON.parse(jsonString)
+    this.expect(results).to.deep.equal(expected)
 })
--- features/support/server.js
+++ features/support/server.js
@@ -3,34 +3,19 @@ var server, httpEndpoint, wsEndpoint
 
 const startServer = () =>
     new Promise((resolve, reject) => {
-        console.log('starting server')
-        try {
-          server = spawn('node', ['./index.js'])
-        } catch (err) {
-          console.log('error starting server')
-          console.error(err)
-        }
-        
+        server = spawn('node', ['./index.js'])
+        server.stdout.on('error', reject)
         server.stdout.on('data', d => {
           let status = d.toString()
-          console.log('stdin data: ', d.toString())
           if (status.match(/Server ready at/)) {
             httpEndpoint = status.replace('Server read at', '').trim()
           } else if (status.match(/Subscriptions ready at/)) {
             wsEndpoint = status.replace('Subscriptions ready at', '').trim()
           }
           if (httpEndpoint && wsEndpoint) {
-            console.log('server started', httpEndpoint, wsEndpoint )
             resolve({ httpEndpoint, wsEndpoint })
           }
         })
-
-        server.stdout.on('error', (error) => {
-          console.log('stdout error')
-          console.error(error)
-        })
-
-        console.log('not sure')
     })
 
 const stopServer = () => {
