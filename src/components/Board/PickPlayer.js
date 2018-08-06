f3bd7dd54207edfc236b49fde7d7a3404ebfdc84
eveporcello <eve@moonhighway.com>
2018-08-06 13:25:34 -0700

    Removes extra punctuation

--- src/components/Board/PickPlayer.js
+++ src/components/Board/PickPlayer.js
@@ -4,25 +4,23 @@ import { PICK_PLAYER, START_GAME } from '.'
 
 export class PickPlayer extends Component {
 
-    comeOnDown = mutation => (
+    comeOnDown = mutation =>
         <div>
             <h1>Come on Down</h1>
             <button onClick={mutation}>Pick Player</button>
         </div>
-    )
 
     maxPlayers = count => 5 - count
 
-    playerPicked = (data, mutation) => (
+    playerPicked = (data, mutation) =>
         <div>
             <h1>{data.pickPlayer.player.login}</h1>
             <img src={data.pickPlayer.player.avatar} alt="" width={100} height={100} />
             <p>Pick {this.maxPlayers(data.pickPlayer.count)} more players</p>
             <button onClick={mutation}>Pick Player</button>
         </div >
-    )
 
-    startGame = () => (
+    startGame = () =>
         <Mutation mutation={START_GAME} update={this.onGameStart}>
             {startGame => {
                 return (
@@ -30,7 +28,6 @@ export class PickPlayer extends Component {
                 )
             }}
         </Mutation>
-    )
 
     onGameStart = () => {
         this.props.history.replace('/board')
