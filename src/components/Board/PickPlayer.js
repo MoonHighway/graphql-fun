f45470c2212b42cc831f22f16a44e27bab2651f5
eveporcello <eve@moonhighway.com>
2018-08-06 11:50:50 -0700

    Start game

--- src/components/Board/PickPlayer.js
+++ src/components/Board/PickPlayer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
 import { gql } from 'apollo-boost'
 import { Mutation } from 'react-apollo'
 
@@ -14,28 +14,65 @@ const PICK_PLAYER = gql`
     }
 `
 
-const countFunction = count => 5 - count
+const START_GAME = gql`
+    mutation start {
+        startGame {
+            playerCount
+        }
+    }
+`
 
 const PlayerPicked = ({ pickPlayer }) => {
     return <div>
         <h1>{pickPlayer.player.login}</h1>
         <img src={pickPlayer.player.avatar} alt="" width={100} height={100} />
-        <p>Pick {countFunction(pickPlayer.count)} more players</p>
     </div >
 }
 
 
-export const PickPlayer = () => (
-    <Mutation mutation={PICK_PLAYER}>
-        {(mutation, { data }) => {
-            console.log(data)
+const maxPlayers = count => 5 - count
+
+const StartGame = () => (
+    <Mutation mutation={START_GAME}>
+        {startGame => {
             return (
-                <div>
-                    {!data ? <h1>Come on Down</h1> : <PlayerPicked {...data} />}
-                    <button onClick={mutation}>Pick Player</button>
-                </div>
+                <button onClick={startGame}>START GAME!</button>
             )
-        }
-        }
-    </Mutation >
+        }}
+    </Mutation>
 )
+
+export class PickPlayer extends Component {
+
+    onStart = async (startGame) => {
+        await startGame()
+        this.props.history.replace('/board')
+    }
+
+    render() {
+        return (
+            <Mutation mutation={PICK_PLAYER}>
+                {(mutation, { data }) => {
+                    return (
+                        <div>
+                            {!data ?
+                                <Fragment>
+                                    <h1>Come on Down</h1>
+                                    <button onClick={mutation}>Pick Player</button>
+                                </Fragment> :
+                                data.pickPlayer.count === 5 ?
+                                    <StartGame /> :
+                                    <Fragment>
+                                        <PlayerPicked {...data} />
+                                        <p>Pick {maxPlayers(data.pickPlayer.count)} more players</p>
+                                        <button onClick={mutation}>Pick Player</button>
+                                    </Fragment>
+                            }
+                        </div>
+                    )
+                }
+                }
+            </Mutation >
+        )
+    }
+}
