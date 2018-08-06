c5802dc47eca78f218e7af4e20650d8aa51cfac1
eveporcello <eve@moonhighway.com>
2018-08-06 12:52:49 -0700

    Adds working redirect for WeJay Screen

--- src/components/Board/PickPlayer.js
+++ src/components/Board/PickPlayer.js
@@ -22,30 +22,28 @@ const START_GAME = gql`
     }
 `
 
-const PlayerPicked = ({ pickPlayer }) => {
-    return <div>
-        <h1>{pickPlayer.player.login}</h1>
-        <img src={pickPlayer.player.avatar} alt="" width={100} height={100} />
-    </div >
-}
-
-
-const maxPlayers = count => 5 - count
-
-const StartGame = () => (
-    <Mutation mutation={START_GAME}>
-        {startGame => {
-            return (
-                <button onClick={startGame}>START GAME!</button>
-            )
-        }}
-    </Mutation>
-)
-
 export class PickPlayer extends Component {
 
-    onStart = async (startGame) => {
-        await startGame()
+    maxPlayers = count => 5 - count
+
+    playerPicked = data => (
+        <div>
+            <h1>{data.pickPlayer.player.login}</h1>
+            <img src={data.pickPlayer.player.avatar} alt="" width={100} height={100} />
+        </div >
+    )
+
+    startGame = () => (
+        <Mutation mutation={START_GAME} update={this.onGameStart}>
+            {startGame => {
+                return (
+                    <button onClick={startGame}>START GAME!</button>
+                )
+            }}
+        </Mutation>
+    )
+
+    onGameStart = () => {
         this.props.history.replace('/board')
     }
 
@@ -61,10 +59,10 @@ export class PickPlayer extends Component {
                                     <button onClick={mutation}>Pick Player</button>
                                 </Fragment> :
                                 data.pickPlayer.count === 5 ?
-                                    <StartGame /> :
+                                    this.startGame() :
                                     <Fragment>
-                                        <PlayerPicked {...data} />
-                                        <p>Pick {maxPlayers(data.pickPlayer.count)} more players</p>
+                                        {this.playerPicked(data)}
+                                        <p>Pick {this.maxPlayers(data.pickPlayer.count)} more players</p>
                                         <button onClick={mutation}>Pick Player</button>
                                     </Fragment>
                             }
