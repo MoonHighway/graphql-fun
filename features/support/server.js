6e78970e959b79f389dea25e0f53d8a5ed86eb9d
MoonTahoe <alex@moonhighway.com>
2018-08-06 15:06:39 -0700

    Adds console logs to start server

--- features/support/server.js
+++ features/support/server.js
@@ -3,19 +3,25 @@ var server, httpEndpoint, wsEndpoint
 
 const startServer = () =>
     new Promise((resolve, reject) => {
+        console.log('starting server')
         server = spawn('yarn', ['start:test'])
         server.stdout.on('data', d => {
           let status = d.toString()
+          console.log('stdin data: ', d.toString())
           if (status.match(/Server ready at/)) {
             httpEndpoint = status.replace('Server read at', '').trim()
           } else if (status.match(/Subscriptions ready at/)) {
             wsEndpoint = status.replace('Subscriptions ready at', '').trim()
           }
           if (httpEndpoint && wsEndpoint) {
+            console.log('server started')
             resolve({ httpEndpoint, wsEndpoint })
           }
         })
-        server.stdout.on('error', reject)
+        server.stdout.on('error', (error) => {
+          console.log('stdout error')
+          console.error(error)
+        })
     })
     .then(d => d.toString())
 
