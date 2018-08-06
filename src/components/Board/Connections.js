3917b58210dbb050315ea4ed7af78032f53a664d
eveporcello <eve@moonhighway.com>
2018-08-06 14:53:52 -0700

    Shows player faces when connected

--- src/components/Board/Connections.js
+++ src/components/Board/Connections.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import { ALL_PLAYERS } from '.'
 
 export const Connections = () =>
-    <h1>Connections</h1>
+    <div>
+        <h1>GraphQL Fun!</h1>
+        <Query query={ALL_PLAYERS} pollInterval={1000}>
+            {({ loading, data }) => {
+                if (loading) return "loading"
+                return data.allPlayers.map(p => <img src={p.avatar} alt="" />)
+            }}
+        </Query>
+    </div>
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -6,6 +6,17 @@ import { Wejay } from './Wejay'
 import { LoadingScreen } from '../ui'
 export * from './PickPlayer'
 
+export const ALL_PLAYERS = gql`
+    query allPlayers {
+        allPlayers {
+            avatar
+            team {
+            color
+            }
+        }
+    }
+`
+
 export const CURRENT_GAME = gql`
     query game {
         currentGame {
