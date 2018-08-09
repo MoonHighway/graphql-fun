e176c417f3ddb765377e0ddce8a4c0b864ab72ff
MoonTahoe <alex@moonhighway.com>
2018-08-08 22:13:24 -0700

    Finalizes the big merge

---
+++ src/components/Player/ui/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import { GraphQLLogo } from '../../ui'
+import AuthorizedPlayer from '../AuthorizedPlayer'
+
+export const Welcome = () =>
+    <Container>
+        <GraphQLLogo />
+        <AuthorizedPlayer />
+    </Container>
+
+
+const Container = styled.div`
+    align-self: stretch;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    font-family: Arial;
+`
