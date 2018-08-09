c71a1f093d733b2038faa4cd6c30c22924332ec5
eveporcello <eve@moonhighway.com>
2018-08-08 21:21:22 -0700

    Adds data paging to allPlayers Query

--- src-api/resolvers/Connections.js
+++ src-api/resolvers/Connections.js
@@ -1,15 +1,23 @@
 export const Query = {
     playerCount: (_, { onDeck = false }, { players, playersOnDeck }) =>
         onDeck ? playersOnDeck.length : players.length,
-    allPlayers: (_, { onDeck = false }, { players, playersOnDeck }) =>
-        onDeck ? playersOnDeck : players
+    allPlayers: (_, { onDeck = false, first }, { players, playersOnDeck }) => {
+        if (first) {
+            var slicedPlayers = players.slice(0, first)
+            return slicedPlayers
+        } else if (onDeck) {
+            return playersOnDeck
+        } else {
+            return players
+        }
+    }
 }
 
 export const Subscription = {
 
     instructions: {
         subscribe(_, args, { pubsub, currentPlayer }) {
-                
+
             if (!currentPlayer) {
                 throw new Error('a player must be logged in to subscribe to instructions')
             }
