be06b1cd0b3665ababe20ba7ab513d60e9d9577a
MoonTahoe <alex@moonhighway.com>
2018-08-10 12:28:54 -0700

    End Screen Showing - still requires style

---
+++ src/components/Player/ui/End.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const End = () =>
+    <Container>
+        <h1>End Screen</h1>
+    </Container>
+
+const Container = styled.div`
+    background-color: red;
+    align-self: stretch;
+`
