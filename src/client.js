ea7849dfde66acd5b589b3fcb01b148cc2b05f0e
MoonTahoe <alex@moonhighway.com>
2018-08-16 11:31:55 -0600

    Gets rid of console args

--- src/client.js
+++ src/client.js
@@ -12,6 +12,20 @@ const storageType = process.env.REACT_APP_TEST_PLAYERS === 'true' ?
     'sessionStorage' : 'localStorage'
 export const storage = window[storageType]
 
+window.printEnv = () => {
+    console.log('\n\nenvironment variables\n=================')
+    console.log('NODE_ENV', process.env.NODE_ENV)
+    console.log('REACT_APP_GRAPHQL_ENDPOINT', process.env.REACT_APP_GRAPHQL_ENDPOINT)
+    console.log('REACT_APP_GRAPHQL_SUBSCRIPTIONS', process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS)
+    console.log('REACT_APP_TEST_PLAYERS', process.env.REACT_APP_TEST_PLAYERS)
+    console.log('REACT_APP_GITHUB_CLIENT_ID', process.env.REACT_APP_GITHUB_CLIENT_ID)
+    console.log('REACT_APP_MAX_CONNECTIONS', process.env.REACT_APP_MAX_CONNECTIONS)
+    console.log('REACT_APP_WEJAY_MAX_FACES', process.env.REACT_APP_WEJAY_MAX_FACES)
+    console.log('storageType: ', storageType)
+    console.log('=================\n\n') 
+    return storage.token
+}
+
 const cache = new InMemoryCache()
 
 //
--- src/index.js
+++ src/index.js
@@ -2,14 +2,4 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
 
-console.log('\n\nenvironment variables\n=================')
-console.log('NODE_ENV', process.env.NODE_ENV)
-console.log('REACT_APP_GRAPHQL_ENDPOINT', process.env.REACT_APP_GRAPHQL_ENDPOINT)
-console.log('REACT_APP_GRAPHQL_SUBSCRIPTIONS', process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS)
-console.log('REACT_APP_TEST_PLAYERS', process.env.REACT_APP_TEST_PLAYERS)
-console.log('REACT_APP_GITHUB_CLIENT_ID', process.env.REACT_APP_GITHUB_CLIENT_ID)
-console.log('REACT_APP_MAX_CONNECTIONS', process.env.REACT_APP_MAX_CONNECTIONS)
-console.log('REACT_APP_WEJAY_MAX_FACES', process.env.REACT_APP_WEJAY_MAX_FACES)
-console.log('=================\n\n')
-
 render(<App />, document.getElementById('root'))
