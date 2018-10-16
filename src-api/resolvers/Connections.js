6d2d01c5f2e59b77f6bb4a81b52a3f9a92ff6b98
Alex Banks <alex@moonhighway.com>
2018-10-16 12:52:57 -0700

    Resolves the Current Payload without monkeypatching (#94)

--- src-api/resolvers/Connections.js
+++ src-api/resolvers/Connections.js
@@ -13,23 +13,28 @@ export const Query = {
     }
 }
 
+var once = true;
+
 export const Subscription = {
 
     instructions: {
+        resolve: (payload, args, { currentPlayer }, info) => currentPlayer,
         subscribe(_, args, { pubsub, currentPlayer }) {
 
             if (!currentPlayer) {
                 throw new Error('a player must be logged in to subscribe to instructions')
             }
 
-            const iterator = pubsub.asyncIterator('new-instructions')
-            const next = iterator.next
-            iterator.next = () => next()
-                .then(() => ({ value: { instructions: currentPlayer } }))
-
-            return iterator
-
+            return pubsub.asyncIterator('new-instructions')
         }
     }
 
 }
+
+
+// returnpubsub.asyncIterator('new-instructions')
+//             const next = iterator.next
+//             iterator.next = () => next()
+//                 .then(() => ({ value: { instructions: currentPlayer } }))
+
+//             return iterator
