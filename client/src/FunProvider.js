ca6f60dd4ad59693c719bfb25f84f05643db97a5
MoonTahoe <alex@moonhighway.com>
2019-09-18 16:19:08 -0700

    Big file move and basic apollo provider setup

---
+++ client/src/FunProvider.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
+import { createHttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+
+const storageType =
+  process.env.REACT_APP_TEST_PLAYERS === "true"
+    ? "sessionStorage"
+    : "localStorage";
+
+export const storage = window[storageType];
+
+const cache = new InMemoryCache();
+const httpLink = createHttpLink({ uri: process.env.REACT_APP_GRAPHQL_URI });
+const authLink = setContext((_, operation) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return {
+      headers: {
+        ...operation.headers,
+        authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    };
+  } else {
+    return operation;
+  }
+});
+
+const httpAuthLink = authLink.concat(httpLink);
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_GRAPHQL_SUBSCRIPTIONS,
+  options: {
+    reconnect: true,
+    lazy: true,
+    connectionParams: () => ({
+      Authorization: storage.token
+    })
+  }
+});
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpAuthLink
+);
+
+const client = new ApolloClient({ cache, link });
+
+export default function FunProvider({ children }) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import { PlayerScreen } from './Player'
+// import { BoardScreen, PickPlayer } from './Board'
+// import { Whoops404 } from './ui'
+
+export default function App() {
+  return <h1>Ready</h1>;
+  //   return (
+  // <BrowserRouter>
+  //     <Switch>
+  //         <Route exact path="/" component={PlayerScreen} />
+  //         <Route exact path="/board" component={BoardScreen} />
+  //         <Route exact path="/pick" component={PickPlayer} />
+  //         <Route component={Whoops404} />
+  //     </Switch>
+  // </BrowserRouter>
+  //   );
+}
