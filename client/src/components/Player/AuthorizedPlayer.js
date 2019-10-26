2c7369e8d571b086f419d5b07afc1fdfd119fdfe
Alex Banks <alex@moonhighway.com>
2019-10-25 18:31:13 -0700

    Asks players if they are there and willing (#158)

--- client/src/components/Player/AuthorizedPlayer.js
+++ client/src/components/Player/AuthorizedPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth } from "../../hooks";
 import { GithubLoginButton, LoadingScreen } from "../ui";
 import { withRouter } from "react-router-dom";
@@ -6,7 +6,69 @@ import { checkForMobile } from "../../lib";
 import { FaMobileAlt } from "react-icons/fa";
 import styled from "styled-components";
 
+const AgreementForm = ({ onAgree = f => f }) => {
+  const [here, setHere] = useState(false);
+  const [nope, setNope] = useState("");
+
+  useEffect(() => {
+    if (!window.performance)
+      setNope(
+        "The performance API was not detected on this mobile browser. In order to participate your browser must have the performance API."
+      );
+  }, []);
+
+  if (nope)
+    return (
+      <Message>
+        <h1>We're sorry...</h1>
+        <p>{nope},</p>
+      </Message>
+    );
+
+  if (!here)
+    return (
+      <Message>
+        <h1>Are you at GraphQL Summit?</h1>
+        <p>
+          You must physically be at GraphQL Summit to participate in the demo.
+        </p>
+        <div className="buttons">
+          <button onClick={e => setHere(here => !here)}>Yes</button>
+          <button
+            onClick={e =>
+              setNope(
+                "You must physically be at GraphQL Summit to participate in the demo."
+              )
+            }
+          >
+            No
+          </button>
+        </div>
+      </Message>
+    );
+
+  return (
+    <Message>
+      <h1>Are you willing to come on stage?</h1>
+      <p>We will pick 4 players at random and ask them to come on stage.</p>
+      <div className="buttons">
+        <button onClick={onAgree}>Yes</button>
+        <button
+          onClick={e =>
+            setNope(
+              "The demo will be used to pick 4 players at random who will be asked to come on stage."
+            )
+          }
+        >
+          No
+        </button>
+      </div>
+    </Message>
+  );
+};
+
 function AuthorizedPlayer({ history }) {
+  const [showAgree, setShowAgree] = useState(false);
   const [isMobile] = useState(
     process.env.REACT_APP_TEST_PLAYERS === "true"
       ? true
@@ -17,6 +79,8 @@ function AuthorizedPlayer({ history }) {
   if (loading) return <LoadingScreen />;
   else if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
+  if (showAgree) return <AgreementForm onAgree={authorize} />;
+
   if (!isMobile) {
     return (
       <Container>
@@ -26,8 +90,21 @@ function AuthorizedPlayer({ history }) {
     );
   }
 
-  return <GithubLoginButton onClick={authorize} />;
+  return (
+    <GithubLoginButton
+      onClick={() => {
+        if (process.env.REACT_APP_TEST_PLAYERS === "true") return authorize();
+        setShowAgree(true);
+      }}
+    />
+  );
 }
+
+const Message = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Container = styled.div`
   width: 500px;
