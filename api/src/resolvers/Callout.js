4527e13e51e01738ee828492e2c1b850f7978681
Alex Banks <alex@moonhighway.com>
2019-10-25 17:51:21 -0700

    Thank you (#157)
    * thankYou mutation added to API
    * Stubs Initial Thank You Screen
    * tweak sound issue

--- api/src/resolvers/Callout.js
+++ api/src/resolvers/Callout.js
@@ -1,10 +1,21 @@
-import { clearCurrentPoll, getCurrentCallout } from "../db";
+import { clearCurrentPoll, getCurrentCallout, startCallout } from "../db";
 
 export const Query = {
   callout: async () => await getCurrentCallout()
 };
 
 export const Mutation = {
+  async thankYou(_, __, { pubsub, isAdmin }) {
+    if (!isAdmin) {
+      throw new Error("only Alex can say thank you");
+    }
+    await startCallout("Thank You", "playing");
+    pubsub.publish("new-instructions");
+    pubsub.publish("callout", {
+      callout: { name: "Thank You", state: "playing" }
+    });
+    return true;
+  },
   async endCallout(_, args, { pubsub }) {
     await clearCurrentPoll();
     pubsub.publish("new-instructions");
@@ -24,5 +35,6 @@ export const Callout = {
     if (parent.name === "Audience Poll") return "AudiencePoll";
     if (parent.name === "Spotlight") return "Spotlight";
     if (parent.name === "Faces") return "Faces";
+    if (parent.name === "Thank You") return "ThankYou";
   }
 };
---
+++ client/src/components/Player/Callouts/ThankYou.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function ThankYou() {
+  return <h1>Thank You : Player</h1>;
+}
